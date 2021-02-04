27a3ae4be17669b677ddb1cea58ee5fd58107612
Arda Ağdemir <ardagdemirr@gmail.com>
2021-02-05 00:27:18 +0300

    Constructor nedir ve nasıl kullanılır?

---
+++ 17-Constructors(Yapıcılar)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _17_Constructors_Yapıcılar_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //Buradaki kod satırına constructor denir. new edilen bir metoda constructor denir.
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "Arda", LastName = "Ağdemir", City = "Ankara" };  //bu şekilde yazılabilir.
+
+            Customer customer2 = new Customer(1, "Arda", "Ağdemir", "Anakra"); //bu şekilde de yazılabilir.
+            //Bğer yukarıdaki(customer2) gibi kullanmak isteniyorsa default customer da bu belirtilmelidir.
+            //Burada bir çıktı alabilmemiz için değerleri set etmemiz gerekir.
+            Console.WriteLine(customer2.FirstName);
+
+
+            Customer customer3 = new Customer(); //Bu yazım ve kullanım şekli, customer1 ile aynıdır. Eğer constructor kullanmak istiyorsak customer2 deki gibi kullanmamız gerekir.
+            customer3.FirstName = "Arda";
+       
+        }
+
+
+        static void Method(int id, string firstName, string lastName, string city)
+        {
+
+        }
+    }
+
+        
+
+
+    class Customer
+    {
+        //bu syntx'a default constructor(parametresi olmayan) denir. Yazılmasa da arka planda çalışır.
+        //bu bir özellik barındıran class türüdür
+        public Customer(int id, string firstName, string lastName, string city) 
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
