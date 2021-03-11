7162a609675f279a634f963a2847b54ad8056b81
Arda Ağdemir <ardagdemirr@gmail.com>
2021-03-11 21:41:50 +0300

    revised

--- 23-LinqMethods/Program.cs
+++ 23-LinqMethods/Program.cs
@@ -8,11 +8,19 @@ namespace _23_LinqMethods
     {//Linq fonksiyonlarını öğrenmek için eğitim bloğu...
         static void Main(string[] args)
         {
+            List<Category> categories = new List<Category>
+            {
+                new Category{CategoryName = "Telefon", CategoryId = 1},
+                new Category{CategoryName = "Televizyon", CategoryId = 2},
+                new Category{CategoryName = "MutfakAletleri", CategoryId = 3}
+            };
+
             List<Product> products = new List<Product>
             {
-                new Product{Name = "Apple Phone", Id = 1, Price=1000},
-                new Product{Name = "Samsung Phone", Id = 2, Price=2000},
-                new Product{Name = "Huawei Phone", Id = 3, Price=2000}
+                new Product{Id = 1, Name = "Apple Phone", Price=1000, CategoryId = 1},
+                new Product{Id = 2, Name = "Samsung Phone", Price=2000, CategoryId = 1},
+                new Product{Id = 3, Name = "Huawei Phone", Price=2000, CategoryId = 1},
+                new Product{Id = 4, Name = "Samsung TV", Price=2000, CategoryId = 2}
             };
             //
             //SINGLE LINE QUERY
@@ -22,13 +30,20 @@ namespace _23_LinqMethods
             var result = products.Any(p => p.Name == "Apple");
             Console.WriteLine(result);
 
+            Console.WriteLine("--------------------");
+
             //Product class ındaki id si 1 olan ürünün türünü bulmamızı sağlar
             var result2 = products.Find(p=> p.Id == 1);
+
             Console.WriteLine(result2.Name);
+
+            Console.WriteLine("--------------------");
 
             //FindAll verilen şarta uyan bütün ürünleri bulmaya yarar. Sonuç liste olarak döner. FindAll yerine Where fonksiyonu da kullanılarak forech ile döndürülebilir.
             var result3 = products.FindAll(p => p.Name.Contains("Pho")); //Contains == string fonksiyonu, isimlerinde ü olanları göstermemize yaramaktadır
             Console.WriteLine(result3);
+
+            Console.WriteLine("--------------------");
 
             //Where fonksiyonu verilen şart doğrultusunda bütün satırları gezerek bir sonuç döndürür.
             var result4 = products.Where(p=> p.Id >= 2);
@@ -37,6 +52,8 @@ namespace _23_LinqMethods
                 Console.WriteLine(product.Id);
             }
 
+            Console.WriteLine("--------------------");
+
             //Where ile bir liste elemanının kendi içerisinde arama yapmak ve bunları sıralamak için aşağıdaki komut yazılabilir
             var result5 = products.Where(p => p.Name.Contains("Pho")).OrderByDescending(p => p.Price).ThenByDescending(p=> p.Name); //adının çerisinde Pho olanları bul ve bunları id ye göre aşağıdan yukarıya sırala
                                                                                                   // ve price ı aynı olanları z' den a'ya filtrele(ThenByDescending)
@@ -44,15 +61,55 @@ namespace _23_LinqMethods
             {
                 Console.WriteLine(product.Name);
             }
+
             Console.WriteLine("--------------------");
-            //farklı bir linq yazımı
-            var result6 = from p in products //products daki her bir üründen(p) price ı 1500 den büyük olanları seç(select)
+
+            //farklı bir linq yazımı -- classic linq
+            var result6 = from p in products //products daki her bir elemandan(p) price ı 1500 den büyük olanları seç(select)
                           where p.Price>1500
                           select p;
             foreach (var product in result6)
             {
                 Console.WriteLine(product.Name);
             }
+
+            Console.WriteLine("--------------------");
+
+            //yukarıdaki gibi her elemandan seçmek yerine ProductDetails ait belirlenmiş elemanlardan seçim yapabiliriz.
+            //select p yerine yeni bir eleman listesi tanımlayabilriz
+            var result7 = from p in products
+                          where p.Price >= 1000
+                          select new ProductDetails { CopyName = p.Name, CopyPrice = p.Price };
+            foreach (var product in result7)
+            {
+                Console.WriteLine(product.CopyName);
+            }
+
+            Console.WriteLine("--------------------");
+
+            //Yukarıda Product class ında var olan özelliklerden filtrelenmiş yeni bir class olan ProductDetails ı kullanarak, Product class ından verileri alabildik.
+            //Ancak Product class ında olmayan, başka bir classta(örn: Category) olan özelliği almak için ne yapmalıyız?
+            //Bunun için linq in "join" özelliğini kullanmamız gerekir.
+            //Öncelikle bir tane nesne oluşturarak(ProductDetails) başlamalıyız. Category ve Product listelerini birbiriyle birleştirmemiz gerekiyor. Bu işleme "ilişkisel tasarım" denir. 
+            var result8 = from p in products
+                          join c in categories
+                          on p.CategoryId equals c.CategoryId //Her iki listede de olan aynı eleman birbiri ile eşlenerek birleştirme yapılmış olur
+                          select new ProductDetails { CopyId = p.Id, CopyName = c.CategoryName, CategoryName = p.Name, CopyPrice = p.Price };
+            foreach (var productDetails in result8)
+            {
+                //Console.WriteLine(productDetails.CopyName + " " + productDetails.CategoryName);
+
+               //Yukarıdakinden farklı bir yazım şekli (string interpolation)
+                Console.WriteLine("{0} ---- {1}", productDetails.CopyName, productDetails.CategoryName);
+            }
+        }
+
+        class ProductDetails
+        {
+            public string CopyName { get; set; }
+            public int CopyId { get; set; }
+            public decimal CopyPrice { get; set; }
+            public string CategoryName { get; set; }
         }
 
         
@@ -62,6 +119,7 @@ namespace _23_LinqMethods
             public string Name { get; set; }
             public int Id { get; set; }
             public decimal Price { get; set; }
+            public int CategoryId { get; set; }
         }
 
 
