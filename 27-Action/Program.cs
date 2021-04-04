f8655e6e83955785f675aa6450dba475c19f5297
Arda Ağdemir <ardagdemirr@gmail.com>
2021-04-04 19:02:26 +0300

    Action

---
+++ 27-Action/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _27_Action
+{
+    class Program
+    {
+        //Action bir metot bloğuna karşılık gelir ve voidleri çalıştırmaya yarayan bir mimaridir.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
